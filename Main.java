2241094064cb25bf77da7e68bc0beed123b8a53e
TheAppleOne <70206641+TheAppleOne@users.noreply.github.com>
2020-09-24 01:21:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("   A   ");
+        System.out.println("  A A  ");
+        System.out.println(" A   A ");
+        System.out.println("A     A");
+        System.out.println("AAAAAAA");
+        System.out.println("A     A");
+        System.out.println("A     A");
+        System.out.println("A     A");
+    }
+}
